7ae45d7df52cdd4b86de93be1cf954330128cd1a
lakshya sharma <84004188+luckybossyt@users.noreply.github.com>
2022-10-25 13:09:59 +0530

    updated the logo color
    updated the logo color made it green 600

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="h-96 flex justify-center items-center">
-      <span className="border-4 p-5 rounded-full border-b-transparent border-green-400 animate-spin"></span>
+      <span className="border-4 p-5 rounded-full border-b-transparent border-green-600 animate-spin"></span>
     </div>
   );
 };
